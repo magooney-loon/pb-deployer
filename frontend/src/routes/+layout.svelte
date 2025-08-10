b630504a7ae81f9e1a9cef248433e247af3c4609
malooney-goon <xxzirrgrizzxx@yahoo.com>
2025-08-10 21:48:53 +0200

    moascno

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -9,7 +9,7 @@
 	<Navigation />
 
 	<!-- Main content -->
-	<main class="mx-auto max-w-7xl px-4 py-8 sm:px-6 lg:px-8">
+	<main class="max-w-9xl mx-auto px-4 py-8 sm:px-6 lg:px-8">
 		{@render children()}
 	</main>
 </div>
